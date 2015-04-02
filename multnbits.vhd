12da2f9fd9e9c75b95625abda1b24ddc2aecaa22
AL3XEY <allemagne7@hotmail.fr>
2015-04-03 01:17:53 +0200

    Add n bits multiplier interface.

---
+++ multnbits.vhd
@@ -0,0 +1,18 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY multnbits IS
+	GENERIC(
+		n : IN NATURAL := 16
+	);
+
+	PORT(
+		a, b : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+		q : OUT STD_LOGIC_VECTOR((2*n)-1 DOWNTO 0);
+	);
+END;
+
+ARCHITECTURE bhv OF multnbits IS
+BEGIN
+	--generate 4 adders & 4 shifters
+END;
