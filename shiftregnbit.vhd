256bb6c02a9167e5fda19e0310dbe4dd706070ce
AL3XEY <allemagne7@hotmail.Fr>
2015-04-07 00:02:21 +0200

    Shift register correction + flipflop & fsm permissions.

--- shiftregnbit.vhd
+++ shiftregnbit.vhd
@@ -16,9 +16,12 @@ ARCHITECTURE bhv OF shiftregnbit IS
 BEGIN
 	PROCESS
 	BEGIN
-		shift : FOR i IN 0 TO n-1 LOOP
-			q(i+1) <= a(n);
-		END LOOP;
+		--shift : FOR i IN 0 TO n-1 LOOP
+		--	q(i+1) <= a(n);
+		--END LOOP;
 		q <= (OTHERS => '0');
+
+		q(n-1 DOWNTO 1) <= a(n-2 DOWNTO 0);
+		q(0) <= '0';
 	END PROCESS;
 END;
