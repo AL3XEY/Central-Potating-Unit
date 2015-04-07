2f56314c6c65423f4276c562baa48793f7be2fd8
AL3XEY <allemagne7@hotmail.fr>
2015-04-07 15:09:30 +0200

    Add file for CPU.

--- cpu16bit.vhd
+++ cpu16bit.vhd
@@ -1,7 +1,7 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
-ENTITY cpu16bits IS
+ENTITY cpu16bit IS
 	PORT(
 		run, resetn, clk : IN STD_LOGIC;
 		din : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -10,7 +10,50 @@ ENTITY cpu16bits IS
 	);
 END;
 
-ARCHITECTURE bhv OF cpu16bits IS
+ARCHITECTURE bhv OF cpu16bit IS
+
+	COMPONENT alunbit IS
+		GENERIC(
+			n : IN NATURAL := 16
+		);
+
+		PORT(
+			a, b : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			sel : IN STD_LOGIC; --operation selection
+			overflow : OUT STD_LOGIC; --if an overflow is detected
+			q : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+		);
+	END COMPONENT;
+
+	COMPONENT controlunit IS
+		PORT(
+			instruction : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
+			run, resetn : IN STD_LOGIC;
+			write : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+			reg_sel, alu_sel, done : OUT STD_LOGIC
+		);
+	END COMPONENT;
+
+	COMPONENT mux10to1 IS
+		PORT(
+			r0, r1, r2, r3, r4, r5, r6, r7, din, alu : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			sig : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			q : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+		);
+	END COMPONENT;
+
+	COMPONENT registernbit IS
+		GENERIC(
+			n : IN NATURAL := 16 
+		);
+
+		PORT(
+			en : IN STD_LOGIC;
+			d : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+			q : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0)
+		);
+	END COMPONENT;
+
 BEGIN
 
 END;
