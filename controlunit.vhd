0501af4ae1a9914f28d06d8440833ed18842c0c8
AL3XEY <allemagne7@hotmail.fr>
2015-04-07 18:25:33 +0200

    Add Control Unit (WIP).

--- controlunit.vhd
+++ controlunit.vhd
@@ -5,7 +5,8 @@ ENTITY controlunit IS
 	PORT(
 		instruction : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
 		run, resetn, clk : IN STD_LOGIC;
-		write, reg_sel : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+		reg_sel : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+		write : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
 		alu_sel : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
 		done : OUT STD_LOGIC
 	);
@@ -22,14 +23,16 @@ BEGIN
 	--if mv or mvi, 1 state
 	--if add, sub or mult, 3 states
 
-	--signal initialization--
+	--signal & output initialization--
 
-	alu_sel <= instruction(7 DOWNT0 6);
+	done <= '0';
+	write <= (OTHERS => '0');
+	alu_sel <= instruction(7 DOWNTO 6);
 	sel_din <= "1000";
 	sel_alu <= "1001";
-	sel_rx(3) <= 0;
+	sel_rx(3) <= '0';
 	sel_rx(2 DOWNTO 0) <= instruction(5 DOWNTO 3);
-	sel_ry(3) <= 0;
+	sel_ry(3) <= '0';
 	sel_rx(2 DOWNTO 0) <= instruction(2 DOWNTO 0);
 
 	wr_a <= "0100000000";
@@ -48,18 +51,28 @@ BEGIN
 
 	--fsm--
 
-	--TODO : clock
-	IF (instruction(2) = '0') THEN --add, sub, mul (alu)
-		--state 1--
-		
-		--state 2--
-		
-		--state 3--
-		
-	ELSIF (instruction(2) = '1') THEN
-		IF (instruction(2) = '0') THEN --mv
-			
-		ELSIF (instruction(2) = '1') THEN --mvi
-	END IF;
+	PROCESS
+	BEGIN
+		--TODO : clock
+		IF (instruction(2) = '0') THEN --add, sub, mul (alu)
+			--state 1--
+			reg_sel <= sel_rx;
+			write <= wr_a;
+			--state 2--
+			reg_sel <= sel_ry;
+			write <= wr_acc;
+			--state 3--
+			reg_sel <= sel_alu;
+			write <= wr_rx;
+		ELSIF (instruction(2) = '1') THEN --else
+			IF (instruction(2) = '0') THEN --mv
+				reg_sel <= sel_ry;
+			ELSIF (instruction(2) = '1') THEN --mvi
+				reg_sel <= sel_din;
+			END IF;
+				write <= wr_rx;
+		END IF;
+		done <= '1';
+	END PROCESS;
 
 END;
