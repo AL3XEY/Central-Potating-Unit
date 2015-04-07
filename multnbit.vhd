0ac569fad9debaa42e6ab9297e49a1b2260f45e2
AL3XEY <allemagne7@hotmail.fr>
2015-04-07 14:56:49 +0200

    Add file for multiplier.

--- multnbit.vhd
+++ multnbit.vhd
@@ -1,18 +1,48 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
-ENTITY multnbits IS
+ENTITY multnbit IS
 	GENERIC(
 		n : IN NATURAL := 16
 	);
 
 	PORT(
 		a, b : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-		q : OUT STD_LOGIC_VECTOR((2*n)-1 DOWNTO 0)
+		overflow : OUT STD_LOGIC; --if an overflow is detected
+		q : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0)
 	);
 END;
 
-ARCHITECTURE bhv OF multnbits IS
+ARCHITECTURE bhv OF multnbit IS
+
+	COMPONENT fulladdernbit IS
+		GENERIC(
+			n : IN NATURAL := 16
+		);
+
+		PORT(
+			a, b : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+			ci : IN STD_LOGIC;
+			q : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+			co : OUT STD_LOGIC
+		);
+	END COMPONENT;
+
+	COMPONENT shiftregnbit IS
+		GENERIC(
+			n : IN NATURAL := 16
+		);
+
+		PORT(
+			a : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+			q : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0)
+		);
+	END COMPONENT;
+
 BEGIN
 	--generate 4 adders & 4 shifters
+
+	overflow <= '0';
+	q <= '0';
+
 END;
